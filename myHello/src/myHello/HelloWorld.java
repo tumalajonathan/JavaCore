0cb30ac5dd5086ca6f98f9dee8a0ca333cebf6a9
user <user@DESKTOP-GD7D4HU>
2019-09-01 00:35:17 +0800

    Basics

---
+++ myHello/src/myHello/HelloWorld.java
@@ -0,0 +1,13 @@
+//My Hello World!!
+package myHello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello world!! ");
+		System.out.print("Jonathan!!");
+		
+	}
+}
