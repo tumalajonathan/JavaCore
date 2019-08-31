58c20089e44143665a87e98f24197ef071220268
user <user@DESKTOP-GD7D4HU>
2019-09-01 00:21:58 +0800

    Java core training

---
+++ GradeBook/src/GradeBook.java
@@ -0,0 +1,9 @@
+//Class Gradebook
+public class GradeBook 
+{
+	//Display Welcome message to the GradeBook user
+	public void displayMessage()
+	{
+		System.out.println("Welcome to the Grade Book!");
+	}
+}
---
+++ GradeBook/src/GradeBookTest.java
@@ -0,0 +1,14 @@
+//Creating Gradebook  Object and calling its displayMessage method
+public class GradeBookTest 
+{
+	//main method begins program execution
+	public static void main(String[] args) {
+		
+		//this is an Object GradeBook
+		GradeBook myGradeBook = new GradeBook();
+		
+		//Display GradeBooks method
+		myGradeBook.displayMessage();
+	}
+
+}
