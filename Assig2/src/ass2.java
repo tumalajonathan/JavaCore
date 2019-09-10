4a4e05056bf0f016692f27eb2d723e451da97440
user <user@DESKTOP-GD7D4HU>
2019-09-10 19:36:18 +0800

    Assignment 2

---
+++ Assig2/src/ass2.java
@@ -0,0 +1,13 @@
+
+public class ass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 4;
+		int b = 36;
+		int sum = a + b;
+		
+		System.out.println("Expected Output: " + sum);
+	}
+
+}
