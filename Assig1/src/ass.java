d3a9ddbc531c429bb8aa425b39b0c2e40b1c9010
user <user@DESKTOP-GD7D4HU>
2019-09-10 19:35:37 +0800

    Assignment 1

---
+++ Assig1/src/ass.java
@@ -0,0 +1,12 @@
+
+public class ass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		System.out.println("Jonathan Tumala");
+
+	}
+
+}
