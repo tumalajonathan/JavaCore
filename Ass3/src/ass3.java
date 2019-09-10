78fbf4f0c9f30c926cf4a0a3447155e5f8db5dc3
user <user@DESKTOP-GD7D4HU>
2019-09-10 19:34:32 +0800

    Assignment 3

---
+++ Ass3/src/ass3.java
@@ -0,0 +1,24 @@
+public class ass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println("before swap");
+		System.out.println("a = " +a);
+		System.out.println("b = " +b);
+		
+		int c = a;
+		
+		a = b;
+		
+		b = c;
+		System.out.println("after swap");
+		System.out.println("a = " +a);
+		System.out.println("b = " +b);
+
+	}
+
+}
